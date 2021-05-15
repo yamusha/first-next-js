8abf4c9caaa4263fedee898335997fb15c539d10
iFarm <mhukdham@gmail.com>
2021-05-15 14:59:22 +0700

    day 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background-color: blue;
+  color: white;
+  border: 2px solid black;
+  padding: 8px;
+`
 
 export default function Home() {
   return (
     <div className={styles.container}>
+    <Button>Submit</Button>
       <h1 style={{color: 'red', border: '1px solid black'}}>Welcome Nextjs</h1>
       <p className="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit qui, cumque officia deserunt totam obcaecati minus quis perferendis porro voluptate praesentium accusantium quod quasi laboriosam dolores est voluptatem tempora aliquam.</p>
       <img src="/images/shopee-LOGO.png" alt="" width="150" />
